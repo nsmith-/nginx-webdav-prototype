482a52331a83eb7bd78148f9136a7ad6280b902f
Andrew Melo <andrew.melo@gmail.com>
2025-03-09 16:29:09 -0500

    Remove debugging
    Don't dump the config json since it may have sensitive info

--- nginx/lua/test_health_content.lua
+++ nginx/lua/test_health_content.lua
@@ -2,11 +2,9 @@ local config = require("config")
 
 -- A health check
 local json = require('cjson')
-json_string = json.encode(config.data)
-print (json_string)
 ngx.status = ngx.HTTP_OK
 if config.data["health_check_id"] and config.data["health_check_id"] ~= nil then
-  ngx.say("OK" .. config.data["health_check_id"])
+  ngx.say("OK " .. config.data["health_check_id"])
 else
   ngx.say("OK")
 end
